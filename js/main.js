c21ad557fba01292447a5997589966b178dc2dd0
Matteo Susanna <matteo2784@gmail.com>
2022-07-06 17:49:57 +0200

    Fine esercizio

--- js/main.js
+++ js/main.js
@@ -2,21 +2,19 @@ const containerDom = document.getElementById('container');
 
 
 for ( let i=1; i < 101; i++ ){
-    containerDom.innerHTML += `<div class="box">${i}</div>`;
-    let boxDom = document.querySelector('.box');
-    
-    
-    if (i % 3 == 0){
-        boxDom.innerHTML = 'fiz';
-    }
 
-    if (i % 5 == 0){
-        boxDom.innerHTML = 'buzz';
-    }
-
+    
     if (i % 5 == 0 && i % 3 == 0){
-        boxDom.innerHTML = 'fizbuzz';
+        containerDom.innerHTML += `<div class="box red">Fizbaz</div>`;
+    } else if (i % 5 == 0){
+        containerDom.innerHTML += `<div class="box yellow">bazz</div>`;
+    } else if (i % 3 == 0){
+        containerDom.innerHTML += `<div class="box green">Fiz</div>`;
+    }else {
+        containerDom.innerHTML += `<div class="box">${i}</div>`;
     }
+
+
 
 
 }
