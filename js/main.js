a01a0a48f38a2f75d897308b4d9b70352136c294
Matteo Susanna <matteo2784@gmail.com>
2022-07-06 16:09:31 +0200

    if

--- js/main.js
+++ js/main.js
@@ -7,16 +7,16 @@ for ( let i=1; i < 101; i++ ){
     
     
     if (i % 3 == 0){
-        boxDom.innerHTML = 'fiz'
-        if (i % 5 == 0){
-            boxDom.innerHTML = 'buzz'
-            if (i % 5 == 0 && i % 3 == 0){
-                boxDom.innerHTML = 'fizbuzz'
-            }
-    
-        }
-        
+        boxDom.innerHTML = 'fiz';
     }
+
+    if (i % 5 == 0){
+        boxDom.innerHTML = 'buzz';
+    }
+
+    if (i % 5 == 0 && i % 3 == 0){
+        boxDom.innerHTML = 'fizbuzz';
+    }
+
+
 }
-
-
