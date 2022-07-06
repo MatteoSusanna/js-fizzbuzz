bd1247b700a87da132f9938ea998c29fb756087e
Matteo Susanna <matteo2784@gmail.com>
2022-07-06 15:29:58 +0200

    .....

--- js/main.js
+++ js/main.js
@@ -5,19 +5,18 @@ for ( let i=1; i < 101; i++ ){
     containerDom.innerHTML += `<div class="box">${i}</div>`;
     let boxDom = document.querySelector('.box');
     
+    
     if (i % 3 == 0){
-        
+        boxDom.innerHTML = 'fiz'
         if (i % 5 == 0){
-            
+            boxDom.innerHTML = 'buzz'
             if (i % 5 == 0 && i % 3 == 0){
-                
+                boxDom.innerHTML = 'fizbuzz'
             }
     
         }
         
     }
-
-    
 }
 
 
