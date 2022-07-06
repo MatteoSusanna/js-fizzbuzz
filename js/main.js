060145062cdd9b23a3442cc45b808d8bfab4dd91
Matteo Susanna <matteo2784@gmail.com>
2022-07-06 15:05:20 +0200

    Fine prima parte

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const containerDom = document.getElementById('container');
+
+
+for ( let i=1; i < 101; i++ ){
+    containerDom.innerHTML += `<div class="box">${i}</div>`;
+    let boxDom = document.querySelector('.box');
+    
+    if (i % 3 == 0){
+        
+        if (i % 5 == 0){
+            
+            if (i % 5 == 0 && i % 3 == 0){
+                
+            }
+    
+        }
+        
+    }
+
+    
+}
+
+
